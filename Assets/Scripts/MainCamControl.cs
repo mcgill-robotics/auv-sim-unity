d783699fbb26c555d47233ea9f7928ad95c9882d
averystraumann <averystraumann@icloud.com>
2023-11-11 13:43:40 -0500

    fix cam controls to please felipe

--- Assets/Scripts/MainCamControl.cs
+++ Assets/Scripts/MainCamControl.cs
@@ -4,30 +4,33 @@ using UnityEngine;
 
 public class MainCamControl : MonoBehaviour
 {
-    public GameObject cam;
+    public Camera cam;
+    public float moveSpeed;
+    public float rotateSpeed;
+    public float scrollSpeed;
+    
+    public GameObject auv;
 
     
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+
     void Update()
     {
-        float xAxisValue = Input.GetAxis("Horizontal");
-        float zAxisValue = Input.GetAxis("Vertical");
-        float yAxisValue = Input.GetAxis("Mouse ScrollWheel");
+
         if(Input.GetMouseButton(0)) {
-		    cam.transform.Rotate(new Vector3(Input.GetAxis("Mouse Y") , -Input.GetAxis("Mouse X"), 0));
+            if (Input.GetAxis("Mouse X") > 0 || Input.GetAxis("Mouse X") < 0) {
+                cam.transform.position += new Vector3(Input.GetAxisRaw("Mouse X") * Time.deltaTime * -moveSpeed,  Input.GetAxisRaw("Mouse Y") * Time.deltaTime * -moveSpeed, 0.0f);
+            }
         }
-        cam.transform.Translate(new Vector3(xAxisValue/15, yAxisValue, zAxisValue/15));
 
-        
-
-
+        if (Input.GetMouseButton(1)) {
+            transform.RotateAround(cam.transform.position,  new Vector3(Input.GetAxisRaw("Mouse Y"),  Input.GetAxisRaw("Mouse X"), 0.0f), rotateSpeed * Time.deltaTime);
+            
+        }
+        Debug.Log(Input.GetAxis("Mouse ScrollWheel"));
+        if (Input.GetAxis("Mouse ScrollWheel") > 0 || Input.GetAxis("Mouse ScrollWheel") < 0 ) {
+            cam.transform.position += (cam.transform.forward * scrollSpeed * Input.GetAxis("Mouse ScrollWheel"));
+         } 
     }
 
 
