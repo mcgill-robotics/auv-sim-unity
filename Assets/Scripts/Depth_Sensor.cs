58a9b90e8ee582dd87f00119d98bedf410b8f8b5
rudolfKischer <rudikischer@gmail.com>
2023-11-25 14:50:56 -0500

    add-depth-sensor-publisher

--- Assets/Scripts/Depth_Sensor.cs
+++ Assets/Scripts/Depth_Sensor.cs
@@ -1,23 +1,37 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
-// using Unity.Robotics.ROSTCPConnector;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RosMessageTypes.Std;
+using Unity.Robotics.ROSTCPConnector;
 
-// /* TO-DO */
+/* TO-DO */
 
-// public class DepthSensor : MonoBehaviour {
-//     ROSConnection roscon;
+public class DepthSensor : MonoBehaviour {
+    ROSConnection roscon;
+    
+    [SerializeField]
+    //publish delay in milliseconds
+    // this should be set to the same as the depth sensor's update rate
+    public float publishDelay = 10;
 
-//     public string pubTopicName = "/SOME_NAME";
+    private string pubTopicName = "/depth";
+    public RosMessageTypes.Std.Float64Msg depth;
 
-//     // Start is called before the first frame update
-//     void Start() {
-//         roscon = ROSConnection.GetOrCreateInstance();
-//         roscon.RegisterPublisher<>(pubTopicName);
-//     }
+    private float lastPublishTime = 0;
 
-//     // Update is called once per frame
-//     void Update() {
-        
-//     }
-// }
+    // Start is called before the first frame update
+    void Start() {
+        roscon = ROSConnection.GetOrCreateInstance();
+        roscon.RegisterPublisher<Float64Msg>(pubTopicName);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Time.time - lastPublishTime < publishDelay / 1000) {
+            return;
+        }
+        depth.data = transform.position.y;
+        roscon.Publish(pubTopicName, depth);
+        lastPublishTime = Time.time;
+    }
+}
