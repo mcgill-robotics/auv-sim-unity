f688883d417c36a1b21969e23dd29924e9345e52
Antoine Dangeard <antoine.dangeard@gmail.com>
2023-12-02 12:51:39 -0500

    add thruster force handling to AUV

--- Assets/Scripts/Thrusters.cs
+++ Assets/Scripts/Thrusters.cs
@@ -5,30 +5,39 @@ using Unity.Robotics.ROSTCPConnector;
 
 public class Thrusters : MonoBehaviour {
     ROSConnection roscon;
+    public Rigidbody auvRb;
+    public Transform[] thrusterPositions;
+    public Vector3[] thrusterDirections;
+    public string thrusterForcesTopicName = "/propulsion/thruster_forces";
 
-    public string subTopicName = "/thruster_report";
+    double[] current_thruster_forces = new double[8];    
 
-    double[] forces = new double[8];    
-
-    void thrusterCallback(RosMessageTypes.Auv.ThrusterForcesMsg msg) {
-        forces[0] = msg.SURGE_PORT;
-        forces[1] = msg.SURGE_STAR;
-        forces[2] = msg.SWAY_BOW;
-        forces[3] = msg.SWAY_STERN;
-        forces[4] = msg.HEAVE_BOW_PORT;
-        forces[5] = msg.HEAVE_BOW_STAR;
-        forces[8] = msg.HEAVE_STERN_STAR;
-        forces[7] = msg.HEAVE_STERN_PORT;
+    void thrusterForceCallback(RosMessageTypes.Auv.ThrusterForcesMsg msg) {
+        current_thruster_forces[0] = msg.SURGE_PORT;
+        current_thruster_forces[1] = msg.SURGE_STAR;
+        current_thruster_forces[2] = msg.SWAY_BOW;
+        current_thruster_forces[3] = msg.SWAY_STERN;
+        current_thruster_forces[4] = msg.HEAVE_BOW_PORT;
+        current_thruster_forces[5] = msg.HEAVE_BOW_STAR;
+        current_thruster_forces[8] = msg.HEAVE_STERN_STAR;
+        current_thruster_forces[7] = msg.HEAVE_STERN_PORT;
     }
 
     // Start is called before the first frame update
     void Start() {
         roscon = ROSConnection.GetOrCreateInstance();
-        roscon.Subscribe<RosMessageTypes.Auv.ThrusterForcesMsg>(subTopicName, thrusterCallback);
+        roscon.Subscribe<RosMessageTypes.Auv.ThrusterForcesMsg>(thrusterForcesTopicName, thrusterForceCallback);
     }
 
     // Update is called once per frame
-    void Update() {
-        
+    void FixedUpdate() {
+        for (int i = 0; i < thrusterPositions.Length; i++) {
+            Vector3 force_in_direction = new Vector3(
+                thrusterDirections[i].x * (float)current_thruster_forces[i],
+                thrusterDirections[i].y * (float)current_thruster_forces[i],
+                thrusterDirections[i].z * (float)current_thruster_forces[i]
+            );
+            auvRb.AddForceAtPosition(force_in_direction, thrusterPositions[i].position, ForceMode.Impulse);
+        }
     }
 }
