84554f377f3353f3b53db386e5c97502cdb70a1a
Antoine Dangeard <antoine.dangeard@gmail.com>
2023-12-09 20:22:43 -0500

    add reference frame indicators to sim

---
+++ Assets/Scripts/MoveReferenceFrameIndicator.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveReferenceFrameIndicator : MonoBehaviour
+{
+
+    void Update()
+    {
+        transform.rotation = Quaternion.Euler(new Vector3(0f,0f,0f));
+    }
+}
