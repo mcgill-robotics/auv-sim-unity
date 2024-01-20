a4d60758046392adcc6da3f0639fa0f909c318c1
frompotenza <potenza.fontes@gmail.com>
2024-01-20 14:32:55 -0500

    expected bearing moves with true bearing except when diana rotates

--- Assets/Scripts/ExpectedPingerBearing.cs
+++ Assets/Scripts/ExpectedPingerBearing.cs
@@ -11,10 +11,10 @@ public class ExpectedPingerBearing : MonoBehaviour {
     private string pingerBearingTopicName = "/sensors/hydrophones/pinger_bearing";
 
     int damping = 5;
-    Vector3 pinger1Bearing;
-    Vector3 pinger2Bearing;
-    Vector3 pinger3Bearing;
-    Vector3 pinger4Bearing;
+    public Vector3 pinger1Bearing;
+    public Vector3 pinger2Bearing;
+    public Vector3 pinger3Bearing;
+    public Vector3 pinger4Bearing;
 
     public Transform Diana;
 
@@ -24,11 +24,16 @@ public class ExpectedPingerBearing : MonoBehaviour {
     public Transform ExpectedPinger4;
 
     void pingerBearingCallback(PingerBearingMsg msg) {
-        pinger1Bearing = new Vector3((float)msg.pinger1_bearing.z, (float)msg.pinger1_bearing.x, (float)msg.pinger1_bearing.y);
-        pinger2Bearing = new Vector3((float)msg.pinger2_bearing.z, (float)msg.pinger2_bearing.x, (float)msg.pinger2_bearing.y);
-        pinger3Bearing = new Vector3((float)msg.pinger3_bearing.z, (float)msg.pinger3_bearing.x, (float)msg.pinger3_bearing.y);
-        pinger4Bearing = new Vector3((float)msg.pinger4_bearing.z, (float)msg.pinger4_bearing.x, (float)msg.pinger4_bearing.y);
+        pinger1Bearing = new Vector3(-(float)msg.pinger1_bearing.y, (float)msg.pinger1_bearing.z, (float)msg.pinger1_bearing.x);
+        pinger2Bearing = new Vector3(-(float)msg.pinger2_bearing.y, (float)msg.pinger2_bearing.z, (float)msg.pinger2_bearing.x);
+        pinger3Bearing = new Vector3(-(float)msg.pinger3_bearing.y, (float)msg.pinger3_bearing.z, (float)msg.pinger3_bearing.x);
+        pinger4Bearing = new Vector3(-(float)msg.pinger4_bearing.y, (float)msg.pinger4_bearing.z, (float)msg.pinger4_bearing.x);
     }
+
+    // unity is ROS
+    // y is z
+    // x is -y
+    // z is x
 
     void Start() {
         roscon = ROSConnection.GetOrCreateInstance();
@@ -41,17 +46,20 @@ public class ExpectedPingerBearing : MonoBehaviour {
         ExpectedPinger3.position = Vector3.MoveTowards(ExpectedPinger3.position, Diana.position, 5.0f);
         ExpectedPinger4.position = Vector3.MoveTowards(ExpectedPinger4.position, Diana.position, 5.0f);
 
+        Debug.Log("BEFORE pinger1Bearing: " + pinger1Bearing);
         pinger1Bearing.y = 0;
         pinger2Bearing.y = 0;
         pinger3Bearing.y = 0;
         pinger4Bearing.y = 0;
-        
+        Debug.Log("AFTER pinger1Bearing: " + pinger1Bearing);
         var rotation1 = Quaternion.LookRotation(pinger1Bearing);
         var rotation2 = Quaternion.LookRotation(pinger2Bearing);
         var rotation3 = Quaternion.LookRotation(pinger3Bearing);
         var rotation4 = Quaternion.LookRotation(pinger4Bearing);
 
-        Debug.Log("Pinger 1 Bearing rotation1: " + rotation1);
+        // Debug.Log("pinger1Bearing: " + pinger1Bearing);
+        // Debug.Log("rotation1: " + rotation1);
+        // Debug.Log("ExpectedPinger1.rotation: " + ExpectedPinger1.rotation);
         
         ExpectedPinger1.rotation = Quaternion.Slerp(ExpectedPinger1.rotation, rotation1, Time.deltaTime * damping);
         ExpectedPinger2.rotation = Quaternion.Slerp(ExpectedPinger2.rotation, rotation2, Time.deltaTime * damping);
