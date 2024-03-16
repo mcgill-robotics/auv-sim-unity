2aa69b54678cab6b632b4ff886868b155e948501
Alb33rt <albert931023@gmail.com>
2024-03-16 14:40:10 -0400

    Changed parameter names, solved array index issues for additional hydrophone

--- Assets/Scripts/PingerTimeDifference.cs
+++ Assets/Scripts/PingerTimeDifference.cs
@@ -13,38 +13,39 @@ public class PingerTimeDifference : MonoBehaviour {
     private PingerTimeDifferenceMsg timeDiffMsg = new PingerTimeDifferenceMsg();
     private double speedOfSound = 1480.0;
 
-    public Transform hydrophone1;
-    public Transform hydrophone2;
-    public Transform hydrophone3;
-    public Transform hydrophone4;
+    public Transform hydrophoneO;
+    public Transform hydrophoneX;
+    public Transform hydrophoneY;
+    public Transform hydrophoneZ;
     public Transform pinger1;
     public Transform pinger2;
     public Transform pinger3;
     public Transform pinger4;
     
+
     Vector3 pinger1Bearing;
     Vector3 pinger2Bearing;
     Vector3 pinger3Bearing;
     Vector3 pinger4Bearing;
 
-    double[] d1 = new double[4];
-    double[] d2 = new double[4];
-    double[] d3 = new double[4];
-    double[] d4 = new double[4];    // for fourth hydrophone
+    double[] dO = new double[4];
+    double[] dX = new double[4];
+    double[] dY = new double[4];
+    double[] dZ = new double[4];    // for fourth hydrophone
 
-    double[] time1 = new double[4];
-    double[] time2 = new double[4];
-    double[] time3 = new double[4];
-    double[] time4 = new double[4]; 
-    double[] time2Diff = new double[4];
-    double[] time3Diff = new double[4];
-    double[] time4Diff = new double[4]; 
+    double[] timeO = new double[4];
+    double[] timeX = new double[4];
+    double[] timeY = new double[4];
+    double[] timeZ = new double[4]; 
+    double[] timeXDiff = new double[4];
+    double[] timeYDiff = new double[4];
+    double[] timeZDiff = new double[4]; 
 
     void Start() {
-        hydrophone1.position = transform.position + Vector3.up * -0.5f;
-        hydrophone2.position = hydrophone1.position + Vector3.forward * 0.1f;
-        hydrophone3.position = hydrophone1.position + Vector3.right * -0.1f;
-        hydrophone4.position = hydrophone1.position + Vector3.up * -0.1f;   // Position unsure, change if incorrect
+        // hydrophoneO.position = transform.position +  Vector3.up * -0.5f;
+        // hydrophoneX.position = hydrophoneO.position + Vector3.forward * 0.1f;
+        // hydrophoneY.position = hydrophoneO.position + Vector3.right * -0.1f;
+        // hydrophoneZ.position =  hydrophoneO.position + Vector3.up * -0.1f;   // Position unsure, change if incorrect
 
         roscon = ROSConnection.GetOrCreateInstance();
         roscon.RegisterPublisher<PingerTimeDifferenceMsg>(pingerTimeDifferenceTopicName); 
@@ -57,70 +58,70 @@ public class PingerTimeDifference : MonoBehaviour {
         timeDiffMsg.is_pinger4_active = true;
         
         // Pinger 1
-        d1[0] = Vector3.Distance(hydrophone1.position, pinger1.position);
-        d2[0] = Vector3.Distance(hydrophone2.position, pinger1.position);
-        d3[0] = Vector3.Distance(hydrophone3.position, pinger1.position);
-        d4[0] = Vector3.Distance(hydrophone4.position, pinger1.position);
+        dO[0] = Vector3.Distance(hydrophoneO.position, pinger1.position);
+        dX[0] = Vector3.Distance(hydrophoneX.position, pinger1.position);
+        dY[0] = Vector3.Distance(hydrophoneY.position, pinger1.position);
+        dZ[0] = Vector3.Distance(hydrophoneZ.position, pinger1.position);
 
-        time1[0] = d1[0] / speedOfSound;
-        time2[0] = d2[0] / speedOfSound;
-        time3[0] = d3[0] / speedOfSound;
-        time4[0] = d4[0] / speedOfSound;
+        timeO[0] = dO[0] / speedOfSound;
+        timeX[0] = dX[0] / speedOfSound;
+        timeY[0] = dY[0] / speedOfSound;
+        timeZ[0] = dZ[0] / speedOfSound;
 
-        time2Diff[0] = time2[0] - time1[0];
-        time3Diff[0] = time3[0] - time1[0];
-        time4Diff[0] = time4[0] - time1[0];
+        timeXDiff[0] = timeO[0] - timeX[0];
+        timeYDiff[0] = timeO[0] - timeY[0];
+        timeZDiff[0] = timeO[0] - timeZ[0];
 
         // Pinger 2
-        d1[1] = Vector3.Distance(hydrophone1.position, pinger2.position);
-        d2[1] = Vector3.Distance(hydrophone2.position, pinger2.position);
-        d3[1] = Vector3.Distance(hydrophone3.position, pinger2.position);
-        d4[1] = Vector3.Distance(hydrophone4.position, pinger2.position);
+        dO[1] = Vector3.Distance(hydrophoneO.position, pinger2.position);
+        dX[1] = Vector3.Distance(hydrophoneX.position, pinger2.position);
+        dY[1] = Vector3.Distance(hydrophoneY.position, pinger2.position);
+        dZ[1] = Vector3.Distance(hydrophoneZ.position, pinger2.position);
 
-        time1[1] = d1[1] / speedOfSound;
-        time2[1] = d2[1] / speedOfSound;
-        time3[1] = d3[1] / speedOfSound;
-        time4[1] = d4[1] / speedOfSound;
+        timeO[1] = dO[1] / speedOfSound;
+        timeX[1] = dX[1] / speedOfSound;
+        timeY[1] = dY[1] / speedOfSound;
+        timeZ[1] = dZ[1] / speedOfSound;
 
-        time2Diff[1] = time2[1] - time1[1];
-        time3Diff[1] = time3[1] - time1[1];
-        time4Diff[1] = time4[1] - time1[1];
+        timeXDiff[1] = timeO[1] - timeX[1];
+        timeYDiff[1] = timeO[1] - timeY[1];
+        timeZDiff[1] = timeO[1] - timeZ[1];
 
         // Pinger 3
-        d1[2] = Vector3.Distance(hydrophone1.position, pinger3.position);
-        d2[2] = Vector3.Distance(hydrophone2.position, pinger3.position);
-        d3[2] = Vector3.Distance(hydrophone3.position, pinger3.position);
-        d4[2] = Vector3.Distance(hydrophone4.position, pinger3.position);
+        dO[2] = Vector3.Distance(hydrophoneO.position, pinger3.position);
+        dX[2] = Vector3.Distance(hydrophoneX.position, pinger3.position);
+        dY[2] = Vector3.Distance(hydrophoneY.position, pinger3.position);
+        dZ[2] = Vector3.Distance(hydrophoneZ.position, pinger3.position);
 
-        time1[2] = d1[2] / speedOfSound;
-        time2[2] = d2[2] / speedOfSound;
-        time3[2] = d3[2] / speedOfSound;
-        time4[2] = d4[2] / speedOfSound;
+        timeO[2] = dO[2] / speedOfSound;
+        timeX[2] = dX[2] / speedOfSound;
+        timeY[2] = dY[2] / speedOfSound;
+        timeZ[2] = dZ[2] / speedOfSound;
 
-        time2Diff[2] = time2[2] - time1[2];
-        time3Diff[2] = time3[2] - time1[2];
-        time4Diff[2] = time4[2] - time1[2];
+        timeXDiff[2] =  timeO[2] - timeX[2];
+        timeYDiff[2] =  timeO[2] - timeY[2];
+        timeZDiff[2] =  timeO[2] - timeZ[2];
 
         // Pinger 4
-        d1[3] = Vector3.Distance(hydrophone1.position, pinger4.position);
-        d2[3] = Vector3.Distance(hydrophone2.position, pinger4.position);
-        d3[3] = Vector3.Distance(hydrophone3.position, pinger4.position);
-        d4[3] = Vector3.Distance(hydrophone4.position, pinger4.position);
+        dO[3] = Vector3.Distance(hydrophoneO.position, pinger4.position);
+        dX[3] = Vector3.Distance(hydrophoneX.position, pinger4.position);
+        dY[3] = Vector3.Distance(hydrophoneY.position, pinger4.position);
+        dZ[3] = Vector3.Distance(hydrophoneZ.position, pinger4.position);
 
-        time1[3] = d1[3] / speedOfSound;
-        time2[3] = d2[3] / speedOfSound;
-        time3[3] = d3[3] / speedOfSound;
-        time4[3] = d4[3] / speedOfSound;
+        timeO[3] = dO[3] / speedOfSound;
+        timeX[3] = dX[3] / speedOfSound;
+        timeY[3] = dY[3] / speedOfSound;
+        timeZ[3] = dZ[3] / speedOfSound;
 
-        time2Diff[3] = time2[3] - time1[3];
-        time3Diff[3] = time3[3] - time1[3];
-        time4Diff[3] = time4[3] - time1[3];
+        timeXDiff[3] = timeO[3] - timeX[3];
+        timeYDiff[3] = timeO[3] - timeY[3];
+        timeZDiff[3] = timeO[3] - timeZ[3];
         
 
-        timeDiffMsg.dt_pinger1 = new double[3] {time2Diff[0], time3Diff[0], time4Diff[0]};
-        timeDiffMsg.dt_pinger2 = new double[3] {time2Diff[1], time3Diff[1], time4Diff[1]};
-        timeDiffMsg.dt_pinger3 = new double[3] {time2Diff[2], time3Diff[2], time4Diff[2]};
-        timeDiffMsg.dt_pinger4 = new double[3] {time2Diff[3], time3Diff[3], time4Diff[3]};
+        timeDiffMsg.dt_pinger1 = new double[3] {timeXDiff[0], timeYDiff[0], timeZDiff[0]};
+        timeDiffMsg.dt_pinger2 = new double[3] {timeXDiff[1], timeYDiff[1], timeZDiff[1]};
+        timeDiffMsg.dt_pinger3 = new double[3] {timeXDiff[2], timeYDiff[2], timeZDiff[2]};
+        timeDiffMsg.dt_pinger4 = new double[3] {timeXDiff[3], timeYDiff[3], timeZDiff[3]};
 
         roscon.Publish(pingerTimeDifferenceTopicName, timeDiffMsg);
     }
