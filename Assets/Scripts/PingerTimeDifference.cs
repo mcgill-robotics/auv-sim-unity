ec54e684c92e59b9141b876e60d32bab776a6803
frompotenza <potenza.fontes@gmail.com>
2024-06-18 20:20:59 -0400

    Fix pub sensors toggles

--- Assets/Scripts/PingerTimeDifference.cs
+++ Assets/Scripts/PingerTimeDifference.cs
@@ -1,104 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-using Unity.Robotics.ROSTCPConnector;
-using Unity.Robotics.ROSTCPConnector.ROSGeometry;
-using RosMessageTypes.Std;
-using RosMessageTypes.Auv;
-
-public class PingerTimeDifference : MonoBehaviour {
-    ROSConnection roscon;
-
-    public string pingerTimeDifferenceTopicName = "/sensors/hydrophones/pinger_time_difference";
-    
-    public double speedOfSound = 1480.0;
-
-    LogicManager1 logicManager1;
-
-    public Transform hydrophoneO;
-    public Transform hydrophoneX;
-    public Transform hydrophoneY;
-    public Transform hydrophoneZ;   // for fourth hydrophone
-    public Transform pinger1;
-    public Transform pinger2;
-    public Transform pinger3;
-    public Transform pinger4;
-
-    public List<Transform> hydrophoneList = new List<Transform>();
-    public List<Transform> pingersList = new List<Transform>();
-
-    Vector3 pinger1Bearing;
-    Vector3 pinger2Bearing;
-    Vector3 pinger3Bearing;
-    Vector3 pinger4Bearing;
-    public int[] frequencies = {1, 2, 3, 4};
-
-    double[] hydrophoneToPingerDistances = new double[4];
-    double[] hydrophoneSignalTimes = new double[4];  
-    // Time difference between Hydrophone 0 and all others
-    double[] hydrophonesTimeDifference = new double[3]; 
-    uint[] timeInNanoseconds = new uint[3];
-
-    void Start() {
-        // hydrophoneO.position = transform.position +  Vector3.up * -0.5f;
-        // hydrophoneX.position = hydrophoneO.position + Vector3.forward * 0.1f;
-        // hydrophoneY.position = hydrophoneO.position + Vector3.right * -0.1f;
-        // hydrophoneZ.position =  hydrophoneO.position + Vector3.up * -0.1f;   // Position unsure, change if incorrect
-
-        hydrophoneList.Add(hydrophoneO);
-        hydrophoneList.Add(hydrophoneX);
-        hydrophoneList.Add(hydrophoneY);
-        hydrophoneList.Add(hydrophoneZ);
-        pingersList.Add(pinger1);
-        pingersList.Add(pinger2);
-        pingersList.Add(pinger3);
-        pingersList.Add(pinger4);
-
-        logicManager1 = FindObjectOfType<LogicManager1>(); // Find an instance of the other class
-
-        roscon = ROSConnection.GetOrCreateInstance();
-        roscon.RegisterPublisher<PingerTimeDifferenceMsg>(pingerTimeDifferenceTopicName);         
-    }
 
 
-    void calculateTimeDifference() {   
-        for (int j = 0; j < pingersList.Count; j++) {  
-            for (int i = 0; i < hydrophoneToPingerDistances.Length; i++) {
-                hydrophoneToPingerDistances[i] = Vector3.Distance(hydrophoneList[i].position, pingersList[j].position);
-                hydrophoneSignalTimes[i] = hydrophoneToPingerDistances[i] / speedOfSound;
-            }   
-            
-            for (int i = 0; i < hydrophonesTimeDifference.Length - 1; i++) {
-                hydrophonesTimeDifference[i] = hydrophoneSignalTimes[0] - hydrophoneSignalTimes[i + 1];
-            }
-            // Debug.Log();
-            // for (int i = 0; i < hydrophonesTimeDifference.Length; i++) {
-            //     // Check if the converted value fits within the uint range (0 to 4294967295)
-            //     if (hydrophonesTimeDifference[i] * 10e-8 <= uint.MaxValue) {
-            //         // Cast the double value multiplied by 10e-8 to uint
-            //         timeInNanoseconds[i] = (uint)(hydrophonesTimeDifference[i] * 10e-8);
-            //     } else {
-            //         // Handle potential overflow if the value is too large for uint
-            //         Debug.LogError($"Time value {hydrophonesTimeDifference[i]} exceeds uint range after conversion. Consider using a different data type.");
-            //     }
-            // }
-            
-            // @TODO - FIX MULTIPLICATION VALUE
-            for (int i = 0; i < hydrophonesTimeDifference.Length; i++) { 
-                timeInNanoseconds[i] = (uint)(hydrophonesTimeDifference[i] * 100000);
-            }
+public class PingerTimeDifference : MonoBehaviour {    
+	double speedOfSound = 1480.0;
 
-            PingerTimeDifferenceMsg timeDiffMsg = new PingerTimeDifferenceMsg();
-            timeDiffMsg.frequency = frequencies[j];
-            
-            timeDiffMsg.times = timeInNanoseconds[..(logicManager1.hydrophonesNumberOption+2)];
-            
-            roscon.Publish(pingerTimeDifferenceTopicName, timeDiffMsg);
-        }
-    }
+	LogicManager1 logicManager1;
 
-    void Update() {
-        calculateTimeDifference();
-    }
+	public Transform hydrophoneO;
+	public Transform hydrophoneX;
+	public Transform hydrophoneY;
+	public Transform hydrophoneZ;   // for fourth hydrophone
+	public Transform pinger1;
+	public Transform pinger2;
+	public Transform pinger3;
+	public Transform pinger4;
+	public int[] frequencies = {1, 2, 3, 4};
+
+	List<Transform> hydrophoneList = new List<Transform>();
+	List<Transform> pingersList = new List<Transform>();
+	
+	void Start() {
+		logicManager1 = FindObjectOfType<LogicManager1>(); // Find an instance of the other class
+		if (logicManager1 == null) {
+			Debug.LogError("[in PingerTimeDifference.cs] LogiManager class is not assigned.");
+		}
+
+		hydrophoneList.Add(hydrophoneO);
+		hydrophoneList.Add(hydrophoneX);
+		hydrophoneList.Add(hydrophoneY);
+		hydrophoneList.Add(hydrophoneZ);
+		pingersList.Add(pinger1);
+		pingersList.Add(pinger2);
+		pingersList.Add(pinger3);
+		pingersList.Add(pinger4);
+	}
+
+	public (uint[], int) calculateTimeDifference(int pinger_index) {   
+		double[] hydrophoneSignalTimes = new double[4];  
+		double[] hydrophoneToPingerDistances = new double[4];
+
+		for (int i = 0; i < hydrophoneToPingerDistances.Length; i++) {
+			hydrophoneToPingerDistances[i] = Vector3.Distance(hydrophoneList[i].position, pingersList[pinger_index].position);
+			hydrophoneSignalTimes[i] = hydrophoneToPingerDistances[i] / speedOfSound;
+		}   
+		
+		// Time difference between Hydrophone 0 and all others.
+		double[] hydrophonesTimeDifference = new double[3]; 
+		for (int i = 0; i < hydrophonesTimeDifference.Length - 1; i++) {
+			hydrophonesTimeDifference[i] = hydrophoneSignalTimes[0] - hydrophoneSignalTimes[i + 1];
+		}
+
+		uint[] timeInNanoseconds = new uint[3];
+		// @TODO - FIX MULTIPLICATION VALUE
+		for (int i = 0; i < hydrophonesTimeDifference.Length; i++) { 
+			timeInNanoseconds[i] = (uint)(hydrophonesTimeDifference[i] * 100000);
+		}
+			
+		return (timeInNanoseconds[..(logicManager1.hydrophonesNumberOption+2)], frequencies[pinger_index]);
+	}
 }
--- Assets/Scripts/StatePublisher.cs
+++ Assets/Scripts/StatePublisher.cs
@@ -1,58 +1,129 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using Unity.Robotics.ROSTCPConnector;
 using Unity.Robotics.ROSTCPConnector.ROSGeometry;
 using System;
 
 public class StatePublisher : MonoBehaviour {
-    private ROSConnection roscon;
-    public string stateTopicName = "/unity/state";
-    public GameObject auv;
+	private ROSConnection roscon;
 
-    private RosMessageTypes.Auv.UnityStateMsg msg = new RosMessageTypes.Auv.UnityStateMsg();
-    private float timeSinceLastUpdate;
-    int isDVLActive = 1;
-    int isDepthSensorActive = 1;
-    int isIMUActive = 1;
-    int isHydrophonesActive = 1; 
-    bool publishToRos = true;
+	LogicManager1 classLogicManager;
+	PingerTimeDifference classPingerTimeDifference;
+	int numberOfPingers = 4;
+	public string stateTopicName = "/unity/state";
+	public GameObject auv;
 
-    int updateFrequency = 10;
+	private RosMessageTypes.Auv.UnityStateMsg msg = new RosMessageTypes.Auv.UnityStateMsg();
+	private float timeSinceLastUpdate;
+	bool isDVLActive;
+	bool isDepthSensorActive;
+	bool isIMUActive;
+	bool isHydrophonesActive; 
+	bool publishToRos;
 
-    // Start is called before the first frame update
-    void Start() {
-      roscon = ROSConnection.GetOrCreateInstance();
-      roscon.RegisterPublisher<RosMessageTypes.Auv.UnityStateMsg>(stateTopicName); 
-    }
+	int updateFrequency = 10;
+
+	// Start is called before the first frame update
+	void Start() {
+		classLogicManager = FindObjectOfType<LogicManager1>(); // Find an instance of the other class
+		if (classLogicManager != null) {
+			SubscribeToggle(classLogicManager.PublishDVLToggle, UpdateisDVLActive);
+			SubscribeToggle(classLogicManager.PublishDepthToggle, UpdateisDepthSensorActive);
+			SubscribeToggle(classLogicManager.PublishIMUToggle, UpdateisIMUActive);
+			SubscribeToggle(classLogicManager.PublishHydrophonesToggle, UpdateisHydrophonesActive);
+		} else {
+			Debug.LogError("[in StatePublisher.cs] LogiManager class is not assigned.");
+		}
+
+		classPingerTimeDifference = FindObjectOfType<PingerTimeDifference>();;
+		if (classPingerTimeDifference == null) {
+			Debug.LogError("[in StatePublisher.cs] PingerTimeDifference class is not assigned.");
+		}
+
+		roscon = ROSConnection.GetOrCreateInstance();
+		roscon.RegisterPublisher<RosMessageTypes.Auv.UnityStateMsg>(stateTopicName); 
+	}
+
+	private void SubscribeToggle(Toggle toggle, Action<bool> updateAction) {
+		if (toggle != null) {
+			// Initialize isActive[...] var with initial toggle value.
+			updateAction(toggle.isOn);
+
+			// Subscribe to the toggle's onValueChanged event.
+			toggle.onValueChanged.AddListener((isOn) => updateAction(isOn));
+		} else {
+			Debug.LogError("Pub Sensor Toggle is not assigned.");
+		}
+	}
+
+	private void UpdateisDVLActive(bool value) {
+		isDVLActive = value;
+	}
+
+	private void UpdateisDepthSensorActive(bool value) {
+		isDepthSensorActive = value;
+	}
+
+	private void UpdateisIMUActive(bool value) {
+		isIMUActive = value;
+	}
+
+	private void UpdateisHydrophonesActive(bool value) {
+		isHydrophonesActive = value;
+	}
+
+	void OnDestroy() {
+		// Unsubscribe from the events to prevent memory leaks.
+		UnsubscribeToggle(classLogicManager.PublishDVLToggle, UpdateisDVLActive);
+		UnsubscribeToggle(classLogicManager.PublishDepthToggle, UpdateisDepthSensorActive);
+		UnsubscribeToggle(classLogicManager.PublishIMUToggle, UpdateisIMUActive);
+		UnsubscribeToggle(classLogicManager.PublishHydrophonesToggle, UpdateisHydrophonesActive);
+	}
+
+	private void UnsubscribeToggle(Toggle toggle, Action<bool> updateAction) {
+		if (toggle != null) {
+			toggle.onValueChanged.RemoveListener((isOn) => updateAction(isOn));
+		}
+	}
 
     // Update is called once per frame
-    void Update() {
-      publishToRos = bool.Parse(PlayerPrefs.GetString("PublishROSToggle", "true"));
-      updateFrequency = int.Parse(PlayerPrefs.GetString("poseRate", "50"));
-      isDVLActive = Convert.ToInt32(bool.Parse(PlayerPrefs.GetString("PublishDVLToggle", "true")));
-      isDepthSensorActive = Convert.ToInt32(bool.Parse(PlayerPrefs.GetString("PublishDepthToggle", "true")));
-      isIMUActive = Convert.ToInt32(bool.Parse(PlayerPrefs.GetString("PublishIMUToggle", "true")));
-      isHydrophonesActive = Convert.ToInt32(bool.Parse(PlayerPrefs.GetString("PublishHydrophonesToggle", "true")));
+	void Update() {
+		publishToRos = bool.Parse(PlayerPrefs.GetString("PublishROSToggle", "true"));
+		updateFrequency = int.Parse(PlayerPrefs.GetString("poseRate", "50"));
 
-      timeSinceLastUpdate += Time.deltaTime;
-      if (timeSinceLastUpdate < 1.0/updateFrequency || !publishToRos) {
-        return;
-      }
-      timeSinceLastUpdate = 0;
+		timeSinceLastUpdate += Time.deltaTime;
+		if (timeSinceLastUpdate < 1.0/updateFrequency || !publishToRos) {
+			return;
+		}
+		timeSinceLastUpdate = 0;
 
-      msg.position = auv.transform.position.To<RUF>();
+		uint[][] times = new uint[numberOfPingers][];
+		int[] frequencies = new int[numberOfPingers];
+		for (int i = 0; i < numberOfPingers; i++) {
+			(uint[] t, int f) = classPingerTimeDifference.calculateTimeDifference(i);
+			times[i] = t;
+			frequencies[i] = f;
+		}
+		
+		msg.position = auv.transform.position.To<RUF>();
 
-      Quaternion rotation = auv.transform.rotation * Quaternion.Euler(0f, 90f, 0f);
-      msg.orientation = rotation.To<NED>();
-      msg.angular_velocity = auv.GetComponent<Rigidbody>().angularVelocity.To<RUF>();
-      msg.velocity = auv.GetComponent<Rigidbody>().velocity.To<RUF>();
+		Quaternion rotation = auv.transform.rotation * Quaternion.Euler(0f, 90f, 0f);
+		msg.orientation = rotation.To<NED>();
+		msg.angular_velocity = auv.GetComponent<Rigidbody>().angularVelocity.To<RUF>();
+		msg.velocity = auv.GetComponent<Rigidbody>().velocity.To<RUF>();
+		msg.frequencies = frequencies;
+		msg.times_pinger_1 = times[0];
+		msg.times_pinger_2 = times[1];
+		msg.times_pinger_3 = times[2];
+		msg.times_pinger_4 = times[3];
 
-      msg.isDVLActive = isDVLActive;
-      msg.isDepthSensorActive = isDepthSensorActive;
-      msg.isIMUActive = isIMUActive;
-      msg.isHydrophonesActive = isHydrophonesActive;
+		msg.isDVLActive = Convert.ToInt32(isDVLActive);
+		msg.isDepthSensorActive = Convert.ToInt32(isDepthSensorActive);
+		msg.isIMUActive = Convert.ToInt32(isIMUActive);
+		msg.isHydrophonesActive = Convert.ToInt32(isHydrophonesActive);
 
-      roscon.Publish(stateTopicName, msg);
-    }
+		roscon.Publish(stateTopicName, msg);
+	}
 }
