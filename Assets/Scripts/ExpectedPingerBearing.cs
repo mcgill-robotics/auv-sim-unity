847ede2ce7d76daaf28b79f96938ec07da64b44f
frompotenza <83185972+frompotenza@users.noreply.github.com>
2024-07-02 16:17:45 -0400

    fix expected hydrophones (#32)

--- Assets/Scripts/ExpectedPingerBearing.cs
+++ Assets/Scripts/ExpectedPingerBearing.cs
@@ -7,7 +7,7 @@ using Unity.Robotics.ROSTCPConnector.ROSGeometry;
 using RosMessageTypes.Std;
 using RosMessageTypes.Auv;
 
-public class ExpectedBearingBearing : MonoBehaviour {
+public class ExpectedBearing : MonoBehaviour {
 	ROSConnection roscon;
 	string pingerBearingTopicName = "/sensors/hydrophones/pinger_bearing";
 	PingerTimeDifference pingerTimeDifference;
@@ -21,7 +21,7 @@ public class ExpectedBearingBearing : MonoBehaviour {
 
 	int[] frequencies = new int[4];
 
-	Quaternion default_rotation = new Quaternion(-0.7071068f, 0f, 0f, 0.7071068f);
+	Quaternion default_rotation = new Quaternion(1.0f, 0f, 0f, 0.0f);
 
 	void Start() {
 		pingerTimeDifference = FindObjectOfType<PingerTimeDifference>(); // Find an instance of the other class
@@ -46,17 +46,16 @@ public class ExpectedBearingBearing : MonoBehaviour {
 		// x is -y
 		// z is x
 		int frequency_index = Array.IndexOf(frequencies, msg.frequency);
-		
-		if (frequency_index == -1) {
-			Debug.LogError("Frequency received mismatch error.");
-			return;
-		} 
 
-		bearingsList[frequency_index].position = new Vector3(
+		Vector3 bearing_adjusted = new Vector3(
 			-(float)msg.pinger_bearing.y, 
 			(float)msg.pinger_bearing.z, 
 			(float)msg.pinger_bearing.x
 		);
+		bearing_adjusted.y = 0;
+		
+		bearingsList[frequency_index].position = Diana.position + new Vector3(0, 1 ,0);
+		bearingsList[frequency_index].rotation = Quaternion.LookRotation(bearing_adjusted) * default_rotation;
 	}
 }
 
