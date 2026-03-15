480584dc45fc19b51d5372c48294b7bbab44fcbd
avyan-k <avyan.kandasamy@gmail.com>
2026-03-14 21:29:25 -0400

    Add DVL JSON from Waterlinked protocol

--- Assets/_Project/Scripts/Sensors/DVLa50SimSender.cs
+++ Assets/_Project/Scripts/Sensors/DVLa50SimSender.cs
@@ -5,9 +5,55 @@ using System.Net.Sockets;
 using System.Text;
 using System.Threading;
 using UnityEngine;
+// from https://docs.waterlinked.com/dvl/dvl-protocol/
+[Serializable]
+struct DVLTransducer
+{
+    public int id { get; set; }
+    public double velocity { get; set; }
+    public double distance { get; set; }
+    public double rssi { get; set; }
+    public double nsi { get; set; }
+    public bool beam_valid { get; set; }
+}
+[Serializable]
+struct DVLVelocityReport
+{
+    public double time { get; set; }
+    public double vx { get; set; }
+    public double vy { get; set; }
+    public double vz { get; set; }
+    public double fom { get; set; }
+    public double[][] covariance { get; set; }
+    public List<DVLTransducer> transducers { get; set; }
+    public bool velocity_valid { get; set; }
+    public byte status { get; set; }
+    public long time_of_validity { get; set; }
+    public long time_of_transmission { get; set; }
+    public string format { get; set; }
+    public string type { get; set; }
+}
+
+[Serializable]
+struct DVLDeadReckoningReport
+{
+    public long ts { get; set; }
+    public double x { get; set; }
+    public double y { get; set; }
+    public double z { get; set; }
+    public double std { get; set; }
+    public double roll { get; set; }
+    public double pitch { get; set; }
+    public double yaw { get; set; }
+    public string type { get; set; }
+    public byte status { get; set; }
+    public string format { get; set; }
+}
+
 
 public class DVLa50SimSender : MonoBehaviour
 {
+    // server components
     TcpListener server; // server to listen for incoming connections
     TcpClient client; // connection to client
     NetworkStream stream; // stream for sending data to client
@@ -20,12 +66,18 @@ public class DVLa50SimSender : MonoBehaviour
     [SerializeField] private string ipAddress; // IP address to bind to
     [SerializeField] private int publishRateHz = 10; // how often to send messages (in Hz)
 
+    // DVL components
+    DVLDeadReckoningReport currentDVLReport;
+    DVLVelocityReport currentVelocityReport;
+
 
     private void Start()
     {
         // Start the server in a separate thread to avoid blocking the main Unity thread
-        serverThread = new Thread(new ThreadStart(SetupServer));
-        serverThread.IsBackground = true; // Ensure the thread will close when the application exits
+        serverThread = new Thread(new ThreadStart(SetupServer))
+        {
+            IsBackground = true // Ensure the thread will close when the application exits
+        };
         isServerRunning = true;
         serverThread.Start();
     }
